5fe3117ec56867af991263d37696ee9077d128fa
toejough <toejough@gmail.com>
2022-06-25 14:26:28 -0400

    GREEN: Add dev tool installation
    **What this does**
    resolves https://github.com/toejough/spacer/issues/13
    
    **Why this is being done**
    To let folks install all the right tools confidently in one command.

---
+++ dev-install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+echo "Checking for fish shell..."
+if ! command -v fish &> /dev/null; then
+    echo "...not found"
+    echo "Checking for brew..."
+    if ! command -v brew &> /dev/null; then
+        echo "...not found"
+        echo "Installing brew"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+
+    echo "Checking for brew..."
+    if ! command -v brew &> /dev/null; then
+        echo "brew installation failed somehow. cannot continue to install dev tooling."
+        exit 1
+    fi
+    echo "...found"
+
+    echo "...installing fish"
+    brew install fish
+
+    echo "Checking for fish shell..."
+    if ! command -v fish &> /dev/null; then
+        echo "...not found"
+        echo "fish installation failed somehow. cannot continue to install dev tooling."
+        exit 1
+    fi
+fi
+echo "...found"
+
+# from https://stackoverflow.com/a/246128
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+$SCRIPT_DIR/dev/dev-install.fish
---
+++ dev/dev-install.fish
@@ -0,0 +1,44 @@
+#! /usr/bin/env fish
+
+function install-if-missing --argument-names cmd pkg-name
+    echo "Checking for $cmd..."
+    if not type -q $cmd
+        echo "...not found"
+        echo "Checking for brew..."
+        if not type -q brew
+            echo "...not found"
+            echo "Installing brew..."
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        end
+        echo "Checking for brew..."
+        if not type -q brew
+            echo "...not found"
+            echo "Unable to continue installing dev tools: brew failed to install."
+            exit 1
+        end
+        echo "...found"
+
+        if test -z $pkg-name
+            set pkg-name $cmd
+        end
+        echo "Installing $pkg-name..."
+        brew install $pkg-name
+
+        echo "Checking for $cmd..."
+        if not type -q $cmd
+            echo "...not found"
+            echo "Unable to continue installing dev tools: $cmd failed to install."
+            exit 1
+        end
+    end
+    echo "...found"
+end
+
+install-if-missing sed gnu-sed
+install-if-missing awk
+install-if-missing ag
+install-if-missing go
+install-if-missing golangci-lint
+
+echo "updating reflex..."
+go install github.com/cespare/reflex@latest
