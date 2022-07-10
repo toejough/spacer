7572dfda989c6423963d8a414396e6ee6e5d0bda
toejough <toejough@gmail.com>
2022-07-09 22:42:27 -0400

    DEV: Make first mock types
    **What this does**
    Makes a mock type for no args, single bool return.
    Makes a mock type for single bool arg, no return.
    
    **Why this is being done**
    To simplify writing and expressing the intent of IO testing.

--- dev/mutate/mutate_internal_test.go
+++ dev/mutate/mutate_internal_test.go
@@ -5,47 +5,116 @@ import (
 	"time"
 )
 
+type Mock struct {
+	t          *testing.T
+	callsChan  chan string
+	resultChan chan bool
+	name       string
+}
+
+func NewMock(t *testing.T, callsChan chan string, name string) *Mock {
+	t.Helper()
+
+	mock := &Mock{
+		t:          t,
+		callsChan:  callsChan,
+		resultChan: make(chan bool),
+		name:       name,
+	}
+
+	return mock
+}
+
+func (m *Mock) ExpectCall() *Mock {
+	m.t.Helper()
+
+	waitForCall(m.t, m.callsChan, m.name)
+
+	return m
+}
+
+func (m *Mock) Return(r bool) *Mock {
+	m.t.Helper()
+
+	setReturnValue(m.t, m.resultChan, r)
+
+	return m
+}
+
+func (m *Mock) Func() bool {
+	m.callsChan <- m.name
+
+	return <-m.resultChan
+}
+
+type MockArgs struct {
+	t         *testing.T
+	callsChan chan string
+	argsChan  chan bool
+	name      string
+}
+
+func NewMockArgs(t *testing.T, callsChan chan string, name string) *MockArgs {
+	t.Helper()
+
+	mock := &MockArgs{
+		t:         t,
+		callsChan: callsChan,
+		argsChan:  make(chan bool),
+		name:      name,
+	}
+
+	return mock
+}
+
+func (m *MockArgs) ExpectCall(b bool) *MockArgs {
+	m.t.Helper()
+
+	waitForCall(m.t, m.callsChan, m.name)
+	waitForArgs(m.t, m.argsChan, b, m.name)
+
+	return m
+}
+
+func (m *MockArgs) Func(args bool) {
+	m.callsChan <- m.name
+	m.argsChan <- args
+}
+
 func TestMainGetsCommand(t *testing.T) {
 	t.Parallel()
 
 	callsChan := make(chan string)
-	reportingArgsChan := make(chan bool)
-	exitArgsChan := make(chan bool)
-	mutateResult := make(chan bool)
 
 	// Given a mutation func
+	mutateMock := NewMock(t, callsChan, "mutate")
 	mutate := func() bool {
-		callsChan <- "mutate"
-
-		return <-mutateResult
+		return mutateMock.Func()
 	}
 
 	// Given a reporting func
+	reportMock := NewMockArgs(t, callsChan, "report")
 	report := func(r bool) {
-		callsChan <- "report"
-		reportingArgsChan <- r
+		reportMock.Func(r)
 	}
 
 	// Given an exit func
-	exit := func(e bool) {
-		callsChan <- "exit"
-		exitArgsChan <- e
+	exitMock := NewMockArgs(t, callsChan, "exit")
+	exit := func(r bool) {
+		exitMock.Func(r)
 	}
 
 	// When run is called
 	go run(mutate, report, exit)
 
 	// Then we run the mutations and return the results
-	waitForCall(t, callsChan, "mutate")
-	setReturnValue(t, mutateResult, true)
+	mutateMock.ExpectCall().Return(true)
 
 	// Then we report the summary of the run with the mutation results
-	waitForCall(t, callsChan, "report")
-	waitForArgs(t, reportingArgsChan, true, "report")
+	reportMock.ExpectCall(true)
 
 	// Then we exit with the result of the mutations
-	waitForCall(t, callsChan, "exit")
-	waitForArgs(t, exitArgsChan, true, "exit")
+	exitMock.ExpectCall(true)
 }
 
 func waitForCall(t *testing.T, callsChan chan string, call string) {
