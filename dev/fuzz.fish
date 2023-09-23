f88fe189b8b67e0e731625bb90135c9909d8e3c4
toejough <toejough@gmail.com>
2023-09-23 13:48:56 -0400

    refactor: Make fuzz test script pass if no fuzz tests found

--- dev/fuzz.fish
+++ dev/fuzz.fish
@@ -3,7 +3,7 @@
 # Workaround to fuzz all the fuzz tests we can write, roughly stolen from https://github.com/golang/go/issues/46312#issuecomment-1153345129
 # Additionally, the use of ag to match a single part of the function was informed by https://github.com/ggreer/the_silver_searcher/issues/400#issuecomment-418454903
 
-set files (ag 'func Fuzz' -G '.*test.go' -l)
+set files (ag 'func Fuzz' -G '.*test.go' -l; or true)
 
 for file in $files
     echo Fuzz tests found in $file
