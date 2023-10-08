791538638ecef3083da70f7a17442aa9d4324541
toejough <toejough@gmail.com>
2023-10-07 22:44:16 -0400

    feat: Implement pretest fetch and run subprocess

--- dev/mutate/mutate.go
+++ dev/mutate/mutate.go
@@ -2,8 +2,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/alexflint/go-arg"
 )
 
 // Mutate. Based loosely on:
@@ -18,9 +23,7 @@ func main() {
 	run(&prodRunDeps{})
 }
 
-type prodPretestDeps struct{}
-
-// Would need testing for this function if we cared too much about the UI.
+// Untested io funcs which need integration testing rather than unit testing.
 func commmonPrintStarting(fname string) func(string) {
 	fmt.Printf("%s is starting...\n", fname)
 
@@ -29,15 +32,60 @@ func commmonPrintStarting(fname string) func(string) {
 	}
 }
 
+// TODO: test the overall flow here - I care about the fetched command being printed to the UI.
+func fetchPretestCommand() []string {
+	done := commmonPrintStarting("fetchPretestCommand")
+	var args struct {
+		PretestCommand []string `arg:"positional,required"`
+	}
+
+	defer func() { done(strings.Join(args.PretestCommand, " ")) }()
+	arg.MustParse(&args)
+
+	return args.PretestCommand
+}
+
+// TODO: test the overall flow here - I care about errors being printed to the UI.
+func runSubprocess(command []string) bool {
+	done := commmonPrintStarting("runSubprocess")
+	var cmd string
+	var args []string
+
+	if len(command) >= 1 {
+		cmd = command[0]
+	}
+	if len(command) >= 2 {
+		args = command[1:]
+	}
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		exitErr := &exec.ExitError{}
+		if errors.As(err, &exitErr) {
+			done(string(exitErr.Stderr))
+			return false
+		}
+		done(err.Error())
+		return false
+	}
+	done("Success")
+	return true
+}
+
+func exit(code int) {
+	os.Exit(code)
+}
+
+// Dependency implementations for tested functions.
+type prodPretestDeps struct{}
+
 func (pd *prodPretestDeps) printStarting(fname string) func(string) {
 	return commmonPrintStarting(fname)
 }
-func (pd *prodPretestDeps) fetchPretestCommand() []string { return []string{} }
-func (pd *prodPretestDeps) runSubprocess([]string) bool   { return true }
+func (pd *prodPretestDeps) fetchPretestCommand() []string       { return fetchPretestCommand() }
+func (pd *prodPretestDeps) runSubprocess(command []string) bool { return runSubprocess(command) }
 
 type prodRunDeps struct{}
 
 func (rd *prodRunDeps) printStarting(fname string) func(string) { return commmonPrintStarting(fname) }
 func (rd *prodRunDeps) pretest() bool                           { return pretest(&prodPretestDeps{}) }
-func (rd *prodRunDeps) testMutations() bool                     { return true }
-func (rd *prodRunDeps) exit(code int)                           { os.Exit(code) }
+func (rd *prodRunDeps) testMutations() bool                     { panic("unimplemented") }
+func (rd *prodRunDeps) exit(code int)                           { exit(code) }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module spacer
 go 1.19
 
 require (
+	github.com/alexflint/go-arg v1.4.3
 	github.com/magefile/mage v1.13.0
 	pgregory.net/rapid v1.1.0
 )
+
+require github.com/alexflint/go-scalar v1.1.0 // indirect
