ec6375b0bce025db9d4ac03ff8aba3a18767d3c7
toejough <toejough@gmail.com>
2024-12-21 20:20:21 -0500

    chore(client): remove dead code

--- web-app/src/pages/IndexPage.vue
+++ web-app/src/pages/IndexPage.vue
@@ -11,27 +11,6 @@
           </q-card>
         </q-item-section>
       </q-item>
-      <template v-for="(note, index) in notes.toReversed()" :key="note">
-        <q-item>
-          <q-item-section>
-            <q-card>
-              <q-card-section v-if="openEditorFor != index" horizontal class="flex justify-between">
-                <q-card-section @click="openEditor(index)">
-                  <span v-sanitize.inline="note"> </span>
-                </q-card-section>
-                <q-card-actions>
-                  <q-btn @click=" remove(index)" round dense flat icon="remove" />
-                </q-card-actions>
-              </q-card-section>
-              <q-card-section v-else horizontal>
-                <q-editor v-model="editorContent" min-height="5rem" class="col" v-on-click-outside="closeEditor" />
-              </q-card-section>
-            </q-card>
-          </q-item-section>
-        </q-item>
-      </template>
-    </q-list>
-    <q-list>
       <draggable :list="draggableNotes" item-key="id" animation=200>
         <template #item="{ element }">
           <q-item>
@@ -59,31 +38,15 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useStorage } from '@vueuse/core'
-// TODO: can I do this with just on-blur?
 import { vOnClickOutside } from '@vueuse/components'
 import draggable from "vuedraggable";
 import { uid } from 'quasar';
 
 const draggableClicked = ref("")
-const notes = useStorage("notes", <string[]>[])
 const newItem = ref("")
 const update = () => {
   draggableNotes.value.unshift({ id: uid(), content: newItem.value })
   newItem.value = ""
-};
-const remove = (index: number) => {
-  const reversed = notes.value.length - index - 1
-  notes.value = notes.value.filter((_, i) => { return reversed != i })
-};
-const openEditorFor = ref(-1)
-const editorContent = ref("")
-const openEditor = (index: number) => {
-  openEditorFor.value = index
-  editorContent.value = notes.value.toReversed()[index] || ""
-};
-const closeEditor = () => {
-  notes.value[notes.value.length - openEditorFor.value - 1] = editorContent.value
-  openEditorFor.value = -1
 };
 const closeDraggableEditor = () => {
   draggableClicked.value = ""
