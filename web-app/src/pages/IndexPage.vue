6f121ae5b768d7c4d5f7abfe889f5185b2cf95a9
toejough <toejough@gmail.com>
2024-12-29 16:29:19 -0500

    wip(client): flashcards list always up to date

--- web-app/src/pages/IndexPage.vue
+++ web-app/src/pages/IndexPage.vue
@@ -64,6 +64,22 @@
           </draggable>
         </q-list>
       </q-tab-panel>
+      <q-tab-panel name="flashcards">
+        <q-list>
+          <div v-for="card in flashcards" :key="card.id">
+            <q-item>
+              <q-item-section>
+                <q-card>
+                  <q-card-section>
+                    <div v-sanitize="card.prompt">
+                    </div>
+                  </q-card-section>
+                </q-card>
+              </q-item-section>
+            </q-item>
+          </div>
+        </q-list>
+      </q-tab-panel>
     </q-tab-panels>
   </q-page>
 </template>
@@ -79,10 +95,16 @@ import { uid } from 'quasar';
 const tabs = ref("notes")
 
 // Flashcards: data
+// TODO: forget the set logic stuff - going to have to implement it manually instead of with the Set type, because we won't be able to compare other state such as last time we practiced, etc.
+// or maybe that's... results? results for a flashcard? Interesting...
 type flashcard = {
+  id: string
   answer: string
   prompt: string
+  noteID: string
 };
+const flashcards = useStorage("flashcards", [] as flashcard[])
+
 // Notes: data
 type draggableNote = {
   id: string;
@@ -94,7 +116,9 @@ const draggableNotes = useStorage("draggableNotes", [] as draggableNote[])
 // Notes: Add/remove note
 const newItem = ref("")
 const update = () => {
-  draggableNotes.value.unshift({ id: uid(), content: newItem.value, flashcards: [] })
+  draggableNotes.value.unshift({
+    id: uid(), content: newItem.value, flashcards: [] as flashcard[]
+  })
   newItem.value = ""
 };
 const removeDraggable = (id: string) => {
@@ -110,23 +134,79 @@ const closeDraggableEditor = () => {
   draggableClicked.value = ""
 };
 const editorOpenedOnNote = (noteId: string) => {
+  // get the note
   draggableClicked.value = noteId
   const index = draggableNotes.value.findIndex((item) => item.id === noteId);
   const note = draggableNotes.value[index];
+  // fix the flashcards, as necessary
   if (note != null) {
-    const regexp = /<b>(.*?)<\/b>/g
-    const array = [...note.content.matchAll(regexp)];
-    const flashcards = array.map((value) => {
-      const input = value.input;
-      const index = value.index;
-      const answer = value[1] || "";
-      const beginning = input.slice(0, index + 3)
-      const blank = "_".repeat(answer.length)
-      const end = input.slice(index + 3 + answer.length)
-      const prompt = beginning + blank + end
-      return { prompt: prompt, answer: answer } as flashcard
-    })
-    note.flashcards = flashcards
+    ensureCardsForNote(note)
+  }
+};
+
+const ensureCardsForNote = (note: draggableNote) => {
+  // identify the correct flashcards
+  const regexp = /<b>(.*?)<\/b>/g
+  const array = [...note.content.matchAll(regexp)];
+  note.flashcards = array.map((value) => {
+    const input = value.input;
+    const index = value.index;
+    const answer = value[1] || "";
+    const beginning = input.slice(0, index + 3)
+    const blank = "_".repeat(answer.length)
+    const end = input.slice(index + 3 + answer.length)
+    const prompt = beginning + blank + end
+    return { prompt: prompt, answer: answer, noteID: note.id, id: note.id + prompt + answer } as flashcard
+  })
+  // if any of these flashcards are not present in the overall list, add them with new id's.
+  const newCards = diffCards(note.flashcards, flashcards.value)
+  flashcards.value.push(...newCards)
+  console.log("adding new cards: ")
+  console.dir(newCards)
+  // if any of the flashcards in the overall list that are pointed at this note don't match, delete them.
+  const globalCardsForThisNote = flashcards.value.filter(card => {
+    return card.noteID == note.id
+  })
+  console.log("global cards for this note: ")
+  console.dir(globalCardsForThisNote)
+  console.log("fset: ")
+  const deadCards = diffCards(globalCardsForThisNote, note.flashcards)
+  removeCardsFrom(flashcards.value, deadCards)
+  // TODO: make adding to the list preserve uniqueness
+  reduceToUnique(flashcards.value)
+};
+
+const diffCards = (base: flashcard[], other: flashcard[]): flashcard[] => {
+  return base.filter(card => {
+    return !other.map(ocard => { return ocard.id }).includes(card.id)
+  })
+};
+
+const removeCardsFrom = (from: flashcard[], toRemove: flashcard[]) => {
+  toRemove.forEach(element => {
+    const index = from.map(card => { return card.id }).indexOf(element.id)
+    if (index > -1) {
+      from.splice(index, 1)
+    }
+  });
+};
+
+const reduceToUnique = (cards: flashcard[]) => {
+  // for each card
+  for (let i = 0; i < cards.length; i++) {
+    // get current card
+    const current = cards[i]
+    if (current === undefined) { break }
+    // search the rest of the list for a match
+    let remainingIndex = cards.slice(i + 1).map(card => { return card.id }).indexOf(current.id)
+    // if found, remove it
+    let found = remainingIndex >= 0
+    while (found) {
+      cards.splice(i + 1 + remainingIndex, 1)
+      remainingIndex = cards.slice(i + 1).map(card => { return card.id }).indexOf(current.id)
+      // if found, remove it
+      found = remainingIndex >= 0
+    }
   }
 };
 
@@ -139,28 +219,13 @@ const toggleFlashCard = () => {
   const index = draggableNotes.value.findIndex((item) => item.id === draggableClicked.value);
   const note = draggableNotes.value[index];
   if (note != null) {
-    const regexp = /<b>(.*?)<\/b>/g
-    const array = [...note.content.matchAll(regexp)];
-    const flashcards = array.map((value) => {
-      const input = value.input;
-      const index = value.index;
-      const answer = value[1] || "";
-      const beginning = input.slice(0, index + 3)
-      const blank = "_".repeat(answer.length)
-      const end = input.slice(index + 3 + answer.length)
-      const prompt = beginning + blank + end
-      return { prompt: prompt, answer: answer } as flashcard
-    })
-    note.flashcards = flashcards
-    // TODO: convert answer/prompt to flashcard
-    // TODO: just add answers/prompts to a flashcards list
-    // TODO: add a flashcards tab
-    // TODO: add flashcard functionality: prompt, answer, remembered, forgot
-    // TODO: add spaced repetition logic
-    // TODO: add section for due vs not
-    // TODO: add notifications
+    ensureCardsForNote(note)
   }
 
+  // TODO: add flashcard functionality: prompt, answer, remembered, forgot
+  // TODO: add spaced repetition logic
+  // TODO: add section for due vs not
+  // TODO: add notifications
   // before doing anything, true up the notes:
   //   if the number of bolded segments != the number of flashcards:
   //     delete the old flashcards
