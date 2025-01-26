48683938bc306c52874a5f69e73840f1e2134a94
toejough <toejough@gmail.com>
2025-01-26 14:08:47 -0500

    wip(client): reorganize index page & add todos

--- web-app/src/pages/IndexPage.vue
+++ web-app/src/pages/IndexPage.vue
@@ -22,21 +22,61 @@
 import { ref } from 'vue'
 import { useStorage } from '@vueuse/core'
 import NoteList from '../components/NoteList.vue'
-import type { draggableNote } from '../components/NoteList.vue'
 import FlashcardList from '../components/FlashcardList.vue'
 import type { flashcard } from '../components/FlashcardList.vue'
 import { uid } from 'quasar';
 import { useNoteCardStore } from 'src/stores/noteCards';
 import { storeToRefs } from 'pinia';
 
+// STORE
 const store = useNoteCardStore();
 
-// dragging with sortablejs
+// NOTECARDS
+// TODO: push this data into the noteList component
+const notes = storeToRefs(store).noteCards
+
+// Sanitizing noteCard data
+// TODO: push this into a store getter
+notes.value.forEach(note => {
+  if (note.id === undefined) { note.id = uid() }
+  if (note.content === undefined) { note.content = "" }
+  if (note.flashcards === undefined) { note.flashcards = [] as flashcard[] }
+  if (note.subnoteIDs === undefined) { note.subnoteIDs = [] as string[] }
+});
+
+// top level notes
+// TODO: these should just be all the notes with no parents. Add parent id's and then calculate this.
+// TODO: put these in the store?
+const defaultIDs = notes.value.map(n => n.id)
+const topLevelNoteIDs = useStorage("topLevelNoteIDs", defaultIDs)
+
+// find notecards that are in the list but are not present in the tree of top-level ID's down.
+const allNoteIDs = notes.value.map(n => n.id)
+const subIDsForID = (id: string): string[] => {
+  const noteIndex = notes.value.findIndex(n => n.id == id)
+  return [id, ...notes.value[noteIndex]?.subnoteIDs.flatMap(subIDsForID) ?? [] as string[]]
+};
+const usedNoteIDs = topLevelNoteIDs.value.flatMap(subIDsForID)
+const missingIDs = allNoteIDs.filter(id => !usedNoteIDs.includes(id))
+// push the notecards that are not in the tree into the top level
+topLevelNoteIDs.value.push(...missingIDs)
+// find the top level id's that are not in the defaultID's list
+const extraIDs = topLevelNoteIDs.value.filter(id => !defaultIDs.includes(id))
+// remove them from the top level?? isn't this just undoing what we just did? all to true up the top level?
+// TODO: replace all of this nonsense with finding the notes with no parents & using them as the top level list.
+extraIDs.forEach(id => {
+  const index = topLevelNoteIDs.value.indexOf(id)
+  topLevelNoteIDs.value.splice(index, 1)
+});
+
 // Tabs
 const tabs = ref("notes")
 
-// Flashcards: data
+// Top level Flashcards
+// TODO: put these into pinia
 // TODO: split out flashcards & results & due dates? these are different concepts...
+// TODO: push this data into the flashcard list component
+const flashcards = useStorage("flashcards", [] as flashcard[])
 
 const checkCards = () => {
   console.log("clicked flashcards")
@@ -46,44 +86,18 @@ const checkCards = () => {
     const index = notes.value.findIndex(note => { return note.id == element.noteID })
     return index >= 0
   })
+  // sanitize remaining flashcard data
+  // TODO: push this into a store getter
   flashcards.value.forEach(e => {
     e.show = e.show === undefined ? false : e.show
     e.due = e.due === undefined ? new Date() : e.due
     e.fibDays = e.fibDays === undefined ? 0 : e.fibDays
     e.forgetfulness = e.forgetfulness === undefined ? 1 : e.forgetfulness
   });
+  // sort cards by due date
   flashcards.value.sort((a: flashcard, b: flashcard): number => { return (new Date(a.due)).getTime() - (new Date(b.due)).getTime() })
 };
 
-const notes = storeToRefs(store).noteCards
-notes.value.forEach(note => {
-  if (note.id === undefined) { note.id = uid() }
-  if (note.content === undefined) { note.content = "" }
-  if (note.flashcards === undefined) { note.flashcards = [] as flashcard[] }
-  if (note.subnoteIDs === undefined) { note.subnoteIDs = [] as string[] }
-});
-const defaultIDs = notes.value.map(n => n.id)
-const topLevelNoteIDs = useStorage("topLevelNoteIDs", defaultIDs)
-console.dir(notes)
-console.dir(topLevelNoteIDs)
-const subIDsForID = (id: string): string[] => {
-  const noteIndex = notes.value.findIndex(n => n.id == id)
-  return [id, ...notes.value[noteIndex]?.subnoteIDs.flatMap(subIDsForID) ?? [] as string[]]
-};
-const allNoteIDs = notes.value.map(n => n.id)
-const usedNoteIDs = topLevelNoteIDs.value.flatMap(subIDsForID)
-console.log(allNoteIDs)
-console.log(usedNoteIDs)
-const missingIDs = allNoteIDs.filter(id => !usedNoteIDs.includes(id))
-console.log(missingIDs)
-const flashcards = useStorage("flashcards", [] as flashcard[])
-topLevelNoteIDs.value.push(...missingIDs)
-const extraIDs = topLevelNoteIDs.value.filter(id => !defaultIDs.includes(id))
-console.log(extraIDs)
-extraIDs.forEach(id => {
-  const index = topLevelNoteIDs.value.indexOf(id)
-  topLevelNoteIDs.value.splice(index, 1)
-});
 </script>
 
 <style lang="sass">
